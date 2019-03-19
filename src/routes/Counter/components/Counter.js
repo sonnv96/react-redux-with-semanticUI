e62873026a09d92265d861a652b121a82c8903c0
Son <sonnv.vnu@gmail.com>
2019-03-19 17:43:17 +0700

    add ui semantic

--- src/routes/Counter/components/Counter.js
+++ src/routes/Counter/components/Counter.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
+import { button } from 'semantic-ui-react'
 export const Counter = ({ counter, increment, doubleAsync }) => (
   <div style={{ margin: '0 auto' }} >
     <h2>Counter: {counter}</h2>
     <button className='btn btn-primary' onClick={increment}>
       Increment
     </button>
+    <button className='ui button'>Click Here</button>
     {' '}
     <button className='btn btn-secondary' onClick={doubleAsync}>
       Double (Async)
